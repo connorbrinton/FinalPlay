c9f180f28c8a6868487e9565a21be8ded8a846ff
Connor Brinton <connor.brinton@gmail.com>
2013-06-28 13:03:19 -0400

    Initial commit of FinalPlay.

---
+++ server/PlayKey.py
@@ -0,0 +1,13 @@
+# https://ws4py.readthedocs.org/en/latest/
+# https://ws4py.readthedocs.org/en/latest/sources/servertutorial/#wsgiref
+
+from wsgiref.simple_server import make_server
+from ws4py.websocket import EchoWebSocket
+from ws4py.server.wsgirefserver import WSGIServer, WebSocketWSGIRequestHandler
+from ws4py.server.wsgiutils import WebSocketWSGIApplication
+
+server = make_server('', 9000, server_class=WSGIServer,
+                     handler_class=WebSocketWSGIRequestHandler,
+                     app=WebSocketWSGIApplication(handler_cls=EchoWebSocket))
+server.initialize_websockets_manager()
+server.serve_forever()
