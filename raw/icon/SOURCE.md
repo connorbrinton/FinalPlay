ffbdd23c7ec9f2830ced92181cef1bcecc666c1d
Connor Brinton <connor.brinton@gmail.com>
2013-07-08 21:48:17 -0400

    Preparation to publish to Chrome Web Store.

---
+++ raw/icon/SOURCE.md
@@ -0,0 +1,3 @@
+Source Files
+=============
+The files in this directory have been derived from http://commons.wikimedia.org/wiki/File:Google_Play_symbol.svg
---
+++ raw/icon/convert.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+inkscape "icon.svg" -h "128" -w "128" -e "../../app/icon_128.png"
+#convert -background none -resize "128x128" "icon.svg" "../../app/icon_128.png"
